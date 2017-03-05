0501b2d8cd6ce4d2038cbd89ce9baac73432c3be
星翼 <853974536@qq.com>
2017-03-05 17:51:55 +0800

    预编译器

---
+++ JunkLang/JunkLang/Precompiler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace JunkLang
+{
+    public class Precompiler
+    {
+        private System.IO.StringReader m_source;
+
+        public void SetSourceCode(string s)
+        {
+            m_source = new StringReader(s);
+        }
+
+        public ArrayList Precompile()
+        {
+            ArrayList codes = new ArrayList();
+
+            while (m_source.Peek() != -1)
+            {
+                string line = m_source.ReadLine();
+                line += ',';
+
+                var comment = line.IndexOf(',');
+
+                line = line.Substring(0, comment);
+                line = line.Trim(" \t\r\n".ToCharArray());
+
+                if (line == null || line == "") continue;
+
+                codes.Add(line);
+            }
+
+            return codes;
+        }
+    }
+}
---
+++ JunkLang/JunkLangTest/PrecompilerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+namespace JunkLangTest
+{
+    [TestClass]
+    public class PrecompilerTest
+    {
+        private void PrecompileAndCreateResult(string testCodeFilePath)
+        {
+            JunkLang.Precompiler precomp = new JunkLang.Precompiler();
+
+            var stream = File.OpenText("..\\..\\" + testCodeFilePath);
+            precomp.SetSourceCode(stream.ReadToEnd());
+
+            foreach(var i in precomp.Precompile())
+              Console.WriteLine("BEGIN["+i+"]END");
+        }
+
+        [TestMethod]
+        public void TestHelloWorld()
+        {
+            PrecompileAndCreateResult("HelloWorld.junk");
+        }
+
+        [TestMethod]
+        public void TestCircle()
+        {
+            PrecompileAndCreateResult("Circle.junk");
+        }
+    }
+}
