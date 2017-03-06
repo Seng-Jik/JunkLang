ee2e4c8f6673747975ce7f943863a7dadafd7e07
星翼 <853974536@qq.com>
2017-03-06 09:21:30 +0800

    罗马数字解析完成

--- JunkLang/JunkLang/RomeNumber.cs
+++ JunkLang/JunkLang/RomeNumber.cs
@@ -13,24 +13,26 @@ namespace JunkLang
             numbers.Add(0);
             foreach(var i in rome)
             {
-                if (rome[i] == 'O') numbers.Add(0);
-                else if (rome[i] == 'I') numbers.Add(1);
-                else if (rome[i] == 'V') numbers.Add(5);
-                else if (rome[i] == 'X') numbers.Add(10);
-                else if (rome[i] == 'L') numbers.Add(50);
-                else if (rome[i] == 'C') numbers.Add(100);
+                if (i == 'O') numbers.Add(0);
+                else if (i == 'I') numbers.Add(1);
+                else if (i == 'V') numbers.Add(5);
+                else if (i == 'X') numbers.Add(10);
+                else if (i == 'L') numbers.Add(50);
+                else if (i == 'C') numbers.Add(100);
                 else throw new Exception("Invalid number:" + rome);
             }
+            numbers.Add(0);
             numbers.Add(0);
 
             byte number = 0;
 
             for(int i = 1;i < numbers.Count - 1; i++)
             {
-                if(numbers[i - 1] > numbers[i])
-                {
+                if (numbers[i] >= numbers[i + 1])
                     number += numbers[i];
-                }
+                
+                else
+                    number -= numbers[i];
             }
             return number;
         }
---
+++ JunkLang/JunkLangTest/RomeNumberTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JunkLang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkLangTest
+{
+    [TestClass()]
+    public class RomeNumberTests
+    {
+        [TestMethod()]
+        public void Rome2ByteTest()
+        {
+            IDictionary < byte,string> dics = new Dictionary<byte, string>();
+            dics[0] = "O";
+            dics[1] = "I";
+            dics[2] = "II";
+            dics[3] = "III";
+            dics[4] = "IV";
+            dics[5] = "V";
+            dics[10] = "X";
+            dics[49] = "IL";
+            dics[51] = "LI";
+            dics[128] = "CXXVIII";
+            dics[255] = "CCLV";
+            
+            foreach(var i in dics)
+            {
+                Assert.AreEqual(RomeNumber.Rome2Byte(i.Value), i.Key);
+            }
+        }
+    }
+}
