978cae4f4252761d71fdc719b2d746817c741e74
星翼 <853974536@qq.com>
2017-03-06 12:31:33 +0800

    修正罗马数字识别

--- JunkLang/JunkLang/RomeNumber.cs
+++ JunkLang/JunkLang/RomeNumber.cs
@@ -9,7 +9,8 @@ namespace JunkLang
         public static byte Rome2Byte(string rome)
         {
             List<byte> numbers = new List<byte>();
-            byte[] times = new byte[255];
+
+            numbers.Add(0);
             foreach (var i in rome)
             {
                 if (i == 'O') numbers.Add(0);
@@ -18,30 +19,20 @@ namespace JunkLang
                 else if (i == 'X') numbers.Add(10);
                 else if (i == 'L') numbers.Add(50);
                 else if (i == 'C') numbers.Add(100);
-                else if (i == 'D') throw new Exception("Out of range");
-                else if (i == 'M') throw new Exception("Out of range");
                 else throw new Exception("Invalid number:" + rome);
-                times[i]++;
             }
-            if (numbers.Count == 0) throw new Exception("Invalid number");
-            if (times['0'] > 1) throw new Exception("Invalid number");
-            if (times['0'] > 0 && (times['I'] > 0 || times['V'] > 0 || times['X'] > 0 || times['L'] > 0 || times['C'] > 0)) throw new Exception("Invalid number");
-            if (times['V'] > 1 || times['L'] > 1 || times['I'] > 3 || times['X'] > 3 || times['C'] > 3) throw new Exception("Rule error");
-            byte before = 0;
+            numbers.Add(0);
+            numbers.Add(0);
+
             byte number = 0;
-            for (int i = 0; i < numbers.Count; i++)
+
+            for (int i = 1; i < numbers.Count - 1; i++)
             {
-                if (numbers[i] > before)
-                {
-                    if ((byte)(int)(numbers[i] - before) < 0) throw new Exception("Error");
-                    before = (byte)(int)(numbers[i] - before);
-                    number = before;
-                }
-                else if (numbers[i] <= before)
-                {
-                    before += numbers[i];
-                    number = before;
-                }
+                if (numbers[i] >= numbers[i + 1])
+                    number += numbers[i];
+
+                else
+                    number -= numbers[i];
             }
             return number;
         }
