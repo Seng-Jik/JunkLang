62beacfbf33c8a4de0e1b181066aedab90cb4666
星翼 <853974536@qq.com>
2017-03-06 10:04:49 +0800

    Parser修正

--- JunkLang/JunkLang/Parser.cs
+++ JunkLang/JunkLang/Parser.cs
@@ -1,14 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Text;
 
 namespace JunkLang
 {
-    class Parser
+    public static class Parser
     {
         public struct Sentence{
             public char command;
-            public int argument;
+            public byte argument;
+        }
+
+        public static IList<Sentence> Parse(IList<Lexer.Token> tokens)
+        {
+            const string cmdNeedArg = ";-+/&_%*~][!|?^v";
+            const string cmdNeedntArg = "#0<>";
+
+            Queue < Lexer.Token > tokenQueue = new Queue<Lexer.Token>(tokens);
+
+            IList<Sentence> sentList = new List<Sentence>();
+           
+            while(tokenQueue.Count > 0)
+            {
+                //取指令
+                var cmd = tokenQueue.Dequeue();
+                Sentence sent = new Sentence();
+
+                if (cmd.type == Lexer.Token.Type.Command)
+                {
+                    sent.command = cmd.word[0];
+
+                    if (cmdNeedArg.IndexOf(sent.command) >= 0)
+                    {
+                        //取参数
+                        var arg = tokenQueue.Dequeue();
+                        if (arg.type == Lexer.Token.Type.Number)
+                        {
+                            sent.argument = RomeNumber.Rome2Byte(arg.word);
+                        }
+                        else
+                            throw new Exception("The argument of " + sent.command + " must is a rome number,but it is " + cmd.word);
+                    }
+                    else if (cmdNeedntArg.IndexOf(sent.command) >= 0)
+                    {
+                        sent.argument = 0;
+                    }
+                    else
+                        throw new Exception("Unknown command:" + sent.command);
+                }
+                else
+                    throw new Exception("It is an unuseable word:" + cmd.word);
+
+                sentList.Add(sent);
+            }
+            return sentList;
         }
     }
 }
