bf1ccb9548107c97739da23b4fc6b108a0d64d95
星翼 <853974536@qq.com>
2017-03-06 11:29:36 +0800

    CSharpCodeGen

---
+++ JunkLang/JunkLangTest/CSharpCodeGenTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JunkLang;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JunkLangTest
+{
+    [TestClass()]
+    public class CSharpCodeGenTests
+    {
+        public static string GenCodeFromJunk(string path)
+        {
+            var sents = ParserTests.ParseFile(path);
+            return CSharpCodeGen.GenCode(sents);
+        }
+
+        private static void PrintCode(string path)
+        {
+            Console.Write(GenCodeFromJunk(path));
+        }
+
+        [TestMethod()]
+        public void GenHelloWorldTest()
+        {
+            PrintCode("HelloWorld.junk");
+        }
+
+        [TestMethod()]
+        public void GenCircleTest()
+        {
+            PrintCode("Circle.junk");
+        }
+    }
+}
